157853609026cf33ba0649b73736e8030cd5ab03
imtiazho <axubair9@gmail.com>
2022-11-22 01:39:54 +0600

    Impliment Add new stock

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // Use Middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.fgazbky.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -30,12 +30,27 @@ async function run() {
     });
 
     // Get Single Product
-    app.get('/shoe/:id', async (req, res) => {
+    app.get("/shoe/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: ObjectId(id) }
+      const query = { _id: ObjectId(id) };
       const product = await shoescollection.findOne(query);
-      res.send(product)
-    })
+      res.send(product);
+    });
+
+    // Edit Specific Product
+    app.put("/shoe/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateUser = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+
+      const updateDoc = {
+        $set: updateUser,
+      };
+
+      const result = await shoescollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
