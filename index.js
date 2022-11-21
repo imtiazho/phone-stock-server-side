a653c3ab5ef3ce8ebfa02ffeb34c887a00e594cd
imtiazho <axubair9@gmail.com>
2022-11-21 18:54:21 +0600

    Impliment single product api

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
+const express = require("express");
+const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-const cors = require('cors')
+const cors = require("cors");
 
 // Use Middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.fgazbky.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try {
-        const shoescollection = client.db("shoesDeal").collection("shoes");
-        await client.connect();
-    }
-    finally {
-        // await client.close();
-    }
+  try {
+    const shoescollection = client.db("shoesDeal").collection("shoes");
+    await client.connect();
+
+    // Get All products
+    app.get("/shoes", async (req, res) => {
+      const query = {};
+      const cursor = shoescollection.find(query);
+      const shoes = await cursor.toArray();
+      res.send(shoes);
+    });
+
+    // Get Single Product
+    app.get('/shoe/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const product = await shoescollection.findOne(query);
+      res.send(product)
+    })
+  } finally {
+    // await client.close();
+  }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Crud is running on port ${port}`)
-})
+  console.log(`Crud is running on port ${port}`);
+});
